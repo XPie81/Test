f09c06e54cbb3d4bbcc00100c46c2db0e19f53ac
XPie81 <c81387@mail.ru>
2025-06-26 11:55:48 +0700

    Реализация нового класса

---
+++ Laba10/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba10
+{
+    internal class TestClass
+    {
+        int testInt;
+        string testString;
+        public TestClass() 
+        {
+            testInt = 1;
+            testString = "test";
+        }
+    }
+}
